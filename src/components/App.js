366c3cdf6d08bcb945ec0c279492b6205f4eac6c
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-06-18 13:28:34 +0530

    submitting code-Sun Jun 18 2023 13:28:33 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
   
-      <label for="gender"><h2>Select your gender</h2></label>
+      {/* <label for="gender"><h2>Select your gender</h2></label>
        <option value="male">Male</option>
        <option value="female">Female</option>
        <label for="shirtSize">Select your shirt size:</label>
@@ -14,8 +14,15 @@ const App = () => {
       <option value="small">Small</option>
       <option value="medium">Medium</option>
       <option value="large">Large</option>
-    </select>
-        {/* Do not remove the main div */}
+    </select> */}
+
+    <h2>Select your gender : </h2>
+    <input type="radio" name="gender" value="male " id="male">  </input>
+    <label forHTML="male"> Male </label>
+    <input type="radio" name="gender" valu="female  "> Female </input>
+
+
+        
     </div>
   )
 }
