0d511b0fcc27e403ec735b0d864244bbce1c8410
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-06-18 12:56:17 +0530

    submitting code-Sun Jun 18 2023 12:56:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
+  
+      <label for="gender"><h2>Select your gender</h2></label>
+       <option value="male">Male</option>
+       <option value="female">Female</option>
+       <label for="shirtSize">Select your shirt size:</label>
+       <select >
+      <option value="small">Small</option>
+      <option value="medium">Medium</option>
+      <option value="large">Large</option>
+    </select>
         {/* Do not remove the main div */}
     </div>
   )
